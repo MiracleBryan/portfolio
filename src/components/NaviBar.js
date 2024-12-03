225a63395bced674e818c4fc94fd7682efe86d60
Ryan W <1643782914@qq.com>
2024-12-03 16:04:31 +1100

    Integrate NaviBar component

--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -1,10 +1,17 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import {useState, useEffect} from "react"
-
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+// import { HashLink } from 'react-router-hash-link';
+// import {
+//   BrowserRouter as Router
+// } from "react-router-dom";
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState("home");
-  const [scolled, setScrolled] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
   useEffect(()=>{
     const onScroll = () => {
@@ -44,9 +51,9 @@ export const NavBar = () => {
               <a href="#"><img src={navIcon2} alt="" /></a>
               <a href="#"><img src={navIcon3} alt="" /></a>
             </div>
-            <HashLink to='#connect'>
+            {/* <HashLink to='#connect'> */}
               <button className="vvd"><span>Let’s Connect</span></button>
-            </HashLink>
+            {/* </HashLink> */}
           </span>
         </Navbar.Collapse>
       </Container>
