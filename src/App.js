06824eac5e83ef6d50ff4eeb127eb9d4e5c57ffe
Ryan W <1643782914@qq.com>
2024-12-05 23:59:50 +1100

    Integrate Project component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {NavBar} from './components/NaviBar'
 import {Banner} from './components/Banner'
 import {Skills} from './components/Skills'
 import {Projects} from './components/Projects'
+import {Contact} from './components/ContactForm'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Banner/>
       <Skills/>
       <Projects/>
+      <Contact/>
     </div>
   );
 }
